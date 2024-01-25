0f11bd41c1c79a3eea1113feec18ac0f0d948817
Luciano Lins <luciano_lins1@hotmail.com>
2024-01-25 20:31:40 -0300

    Feat:Login Form Component

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './LoginForm.css'
+import ImageLogin from '/assets/image-login.jpg'
+
+const LoginForm = () => {
+    return (
+        <div className="wrapper">
+            <img src={ImageLogin} alt="" />
+            <form className="sign-in" action="">
+            
+                <h2>Sign Up</h2>
+                <div className="input-box">
+                    <input type="text" placeholder="Nome" required/>
+                </div>
+                <div className="input-box">
+                    <input type="e-mail" placeholder="Email" required/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Senha" required/>
+                </div>
+
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />Lembrar-se</label>
+                    <a href="#">Esqueci a Senha</a>
+                </div>
+
+                <button type="submit">Sign In</button>
+
+
+            </form>
+        </div>
+    )
+}
+export default LoginForm
